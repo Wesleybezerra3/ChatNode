c47ec5b9d03a662afbf7e83ba191b5cf3607853e
Wesleybezerra3 <wesleybezerra770@gmail.com>
2025-04-05 22:40:10 -0300

    deployfull ...

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import {io} from'socket.io-client';
 
-const socket = io('http://localhost:8180',{
+const socket = io('https://chatnode-dxiq.onrender.com/',{
     transports: ["websocket"], // Força WebSocket em vez de polling
 });
 
